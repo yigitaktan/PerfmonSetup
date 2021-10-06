b3813bc0fca13a088d8cfed1ca4f1bbefcd07533
Yigit Aktan <51110247+yigitaktan@users.noreply.github.com>
2021-10-06 10:03:08 +0300

    Update functions.psm1
    Some controls were added

--- functions.psm1
+++ functions.psm1
@@ -1,4 +1,4 @@
-﻿<# 
+<# 
 ╔═════════════════════════════════════════════════════════════════════════════╗
 ║DEVELOPER DOES NOT WARRANT THAT THE SCRIPT WILL MEET YOUR NEEDS OR BE FREE   ║
 ║FROM ERRORS, OR THAT THE OPERATIONS OF THE SOFTWARE WILL BE UNINTERRUPTED.   ║
@@ -11,29 +11,31 @@
 └─────────┴───────────────────────────────────────────────────────────────────┘
 #>
 
+
+
 Function Start-DataCollectorSet
 {
- if ($StartCollectorCheck -eq "y")
+ If ($StartCollectorCheck -eq "y")
   {
-   if ($SingleSelection -eq "y") {Write-Host "[*] Data Collector Set starting for" $SelectedInstance -ForegroundColor Yellow}
+   If ($SingleSelection -eq "y") {Write-Host "[*] Data Collector Set starting for" $SelectedInstance -ForegroundColor Yellow}
    Start-Sleep -Seconds 5
 
-   for ($i=0; $i -lt $StartCollectorList.Count; $i++)
+   For ($i=0; $i -lt $StartCollectorList.Count; $i++)
     {
-     if ($SingleSelection -eq "n") {Write-Host "[*] Data Collector Set starting for" $StartCollectorList[$i] -ForegroundColor Yellow}
+     If ($SingleSelection -eq "n") {Write-Host "[*] Data Collector Set starting for" $StartCollectorList[$i] -ForegroundColor Yellow}
      $logmanstartparam = "start " + $StartCollectorList[$i] + "_SfMC_Counter_Set"
      Start-Process -WindowStyle hidden -FilePath "logman.exe" -ArgumentList $logmanstartparam 
    
-     Delete-CounterFile
-  } 
+     Delete-CounterFile 
+    } 
    $(AllDone)
-}
-elseif ($StartCollectorCheck -eq "n")
-   {
-    Start-Sleep -Seconds 7
-    Delete-CounterFile
-    AllDone
-   }
+  }
+ ElseIf ($StartCollectorCheck -eq "n")
+  {
+   Start-Sleep -Seconds 7
+   Delete-CounterFile
+   AllDone
+  }
 }
 
 
@@ -62,16 +64,16 @@ Function Get-SqlInstances
      {
       $temp = $caption | %{$_.split(" ")[-1]} | %{$_.trimStart("(")} | %{$_.trimEnd(")")}
 
-      $SqlInstanceMenuArrayList.Add("$count) $ServerName\$temp") > $null
-      $SqlInstanceArrayList.Add("$temp") > $null
+      $global:SqlInstanceMenuArrayList.Add("$count) $ServerName\$temp") > $null
+      $global:SqlInstanceArrayList.Add("$temp") > $null
      }
   }
   $TotalInstanceCount = $captions.Count
   if ($captions.Count -gt 1)
    {
     $global:SingleInstanceCheck = "n"
-    $SqlInstanceMenuArrayList.Add("$all) All") > $null
-    $SqlInstanceArrayList.Add("All") > $null
+    $global:SqlInstanceMenuArrayList.Add("$all) All") > $null
+    $global:SqlInstanceArrayList.Add("All") > $null
     $global:IsSQL = "yes"
    }
   if ($captions.Count -eq 1)
@@ -111,3 +113,74 @@ Function Delete-BlgFile([String] $Instance)
    Get-ChildItem -Path $LogFilePath | Where-Object {$_.name -match $FileName} | Remove-Item
   }
 }
+
+
+Function Create-DataCollectorSet
+ {
+  If (($global:SqlInstanceArrayList[$global:InstanceNumber - 1] -ne "All") -and ($global:SqlInstanceArrayList[$global:InstanceNumber - 1] -ne "MSSQLSERVER")) #If single named instance selected   
+   {
+    $global:SelectedInstance = $global:SqlInstanceArrayList[$global:InstanceNumber - 1]
+    ((Get-Content -path $PSScriptRoot\counterset.txt -Raw) -replace '\[MYINSTANCENAME]:',('MSSQL$'+$global:SelectedInstance+':')) | Set-Content -Path $PSScriptRoot\$global:SelectedInstance.txt
+ 
+    Delete-BlgFile -Instance $global:SelectedInstance
+                        
+    $logmanparam = "CREATE COUNTER -n " + $global:SelectedInstance + "_SfMC_Counter_Set -s . -cf " + $PSScriptRoot + "\" + $global:SelectedInstance + ".txt -f bincirc -max " + $MaxLogFileSize + " -si " + $IntervalNumber + " -o " + $global:LogFilePath + "\" + $global:SelectedInstance + "_sfmc_perfmon.blg"                 
+    Start-Process -WindowStyle hidden -FilePath "logman.exe" -ArgumentList $logmanparam
+
+    $global:SingleSelection = "y"
+                    
+    If ($global:StartCollectorList -notcontains $global:SelectedInstance){ $global:StartCollectorList.Add($global:SelectedInstance) > $null }
+    Write-Host "[*] Data Collector Set creating for" $global:SelectedInstance -ForegroundColor DarkYellow
+   } 
+  ElseIf ($global:SqlInstanceArrayList[$global:InstanceNumber - 1] -eq "MSSQLSERVER") #If default instance selected
+   {
+    $global:SelectedInstance = $global:SqlInstanceArrayList[$global:InstanceNumber - 1]
+    ((Get-Content -path $PSScriptRoot\counterset.txt -Raw) -replace '\[MYINSTANCENAME]:','SQLServer:') | Set-Content -Path $PSScriptRoot\$global:SelectedInstance.txt
+
+    Delete-BlgFile -Instance $global:SelectedInstance
+
+    $logmanparam = "CREATE COUNTER -n " + $global:SelectedInstance + "_SfMC_Counter_Set -s . -cf " + $PSScriptRoot + "\" + $global:SelectedInstance + ".txt -f bincirc -max " + $MaxLogFileSize + " -si " + $IntervalNumber + " -o " + $global:LogFilePath + "\" + $global:SelectedInstance + "_sfmc_perfmon.blg"                              
+    Start-Process -WindowStyle hidden -FilePath "logman.exe" -ArgumentList $logmanparam
+
+    $global:SingleSelection = "y"
+                    
+    If ($global:StartCollectorList -notcontains $global:SelectedInstance){ $global:StartCollectorList.Add($global:SelectedInstance) > $null }
+    Write-Host "[*] Data Collector Set creating for" $global:SelectedInstance -ForegroundColor DarkYellow
+   }
+  ElseIf (($global:SqlInstanceArrayList[$global:InstanceNumber - 1] = "All") -and ($SingleInstanceCheck -eq "n")) #If all instances selected
+   {
+    $global:SingleSelection = "n"
+    For ($i=0; $i -lt $global:SqlInstanceArrayList.Count-1; $i++) 
+     {
+      $filename = $PSScriptRoot + "\" + $global:SqlInstanceArrayList[$i] + ".txt"
+      $global:SelectedInstance = "All"
+
+      If ($global:StartCollectorList -notcontains $global:SqlInstanceArrayList[$i]){ $global:StartCollectorList.Add($global:SqlInstanceArrayList[$i]) > $null }
+      Write-Host "[*] Data Collector Set creating for" $global:SqlInstanceArrayList[$i] -ForegroundColor DarkYellow
+
+      If ($global:SqlInstanceArrayList[$i] -eq "MSSQLSERVER")
+       {
+        ((Get-Content -path $PSScriptRoot\counterset.txt -Raw) -replace '\[MYINSTANCENAME]:','SQLServer:') | Set-Content -Path $filename
+
+        Delete-BlgFile -Instance $global:SqlInstanceArrayList[$i]
+
+        $logmanparam = "CREATE COUNTER -n MSSQLSERVER_SfMC_Counter_Set -s . -cf " + $PSScriptRoot + "\MSSQLSERVER.txt -f bincirc -max " + $MaxLogFileSize + " -si " + $IntervalNumber + " -o " + $global:LogFilePath + "\MSSQLSERVER_sfmc_perfmon.blg"                 
+        Start-Process -WindowStyle hidden -FilePath "logman.exe" -ArgumentList $logmanparam
+            
+        If ($global:StartCollectorList -notcontains 'MSSQLSERVER'){ $global:StartCollectorList.Add("MSSQLSERVER") > $null }
+       }
+      Else
+       {
+        ((Get-Content -path $PSScriptRoot\counterset.txt -Raw) -replace '\[MYINSTANCENAME]:',('MSSQL$'+$global:SqlInstanceArrayList[$i]+':')) | Set-Content -Path $filename
+
+        Delete-BlgFile -Instance $global:SqlInstanceArrayList[$i]
+
+        $logmanparam = "CREATE COUNTER -n " + $global:SqlInstanceArrayList[$i] + "_SfMC_Counter_Set -s . -cf " + $PSScriptRoot +"\" + $global:SqlInstanceArrayList[$i] + ".txt -f bincirc -max " + $MaxLogFileSize + " -si " + $IntervalNumber + " -o " + $global:LogFilePath + "\" + $global:SqlInstanceArrayList[$i] + "_sfmc_perfmon.blg"                 
+        Start-Process -WindowStyle hidden -FilePath "logman.exe" -ArgumentList $logmanparam
+                     
+        If ($global:StartCollectorList -notcontains $global:SqlInstanceArrayList[$i]){ $global:StartCollectorList.Add($global:SqlInstanceArrayList[$i]) > $null }
+       }
+      }              
+     }
+  Start-DataCollectorSet
+ }
